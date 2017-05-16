514f55177102ba4f72472c18eff4f405528ef2f6
Suryakant <suryknt@gmail.com>
2017-05-16 13:18:54 +0530

    Added machine learning assignments

---
+++ MachineLearning/mlCourse/workspace/week7/machine-learning-ex6/ex6/dataset3Params.m
@@ -0,0 +1,39 @@
+function [C, sigma] = dataset3Params(X, y, Xval, yval)
+%EX6PARAMS returns your choice of C and sigma for Part 3 of the exercise
+%where you select the optimal (C, sigma) learning parameters to use for SVM
+%with RBF kernel
+%   [C, sigma] = EX6PARAMS(X, y, Xval, yval) returns your choice of C and 
+%   sigma. You should complete this function to return the optimal C and 
+%   sigma based on a cross-validation set.
+%
+
+% You need to return the following variables correctly.
+C = 1;
+sigma = 0.1;
+clist = [0.01 0.03 0.1 0.3 1 3 10  ];
+sigmalist = [0.01 0.03 0.1 0.3 1 3 10  ];
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Fill in this function to return the optimal C and sigma
+%               learning parameters found using the cross validation set.
+%               You can use svmPredict to predict the labels on the cross
+%               validation set. For example, 
+%                   predictions = svmPredict(model, Xval);
+%               will return the predictions on the cross validation set.
+%
+%  Note: You can compute the prediction error using 
+%        mean(double(predictions ~= yval))
+%
+
+%[v ind]=min(err);
+%[vv ii]=min(v);
+%C=clist(ii);
+%sigma=sigmalist(ind(ii));
+%[C sigma]
+
+
+
+
+% =========================================================================
+
+end
